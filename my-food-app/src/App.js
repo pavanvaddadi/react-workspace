6c082655f92b71de6078053f482093ac40cb5ccc
pavanvaddadi <pavanvaddadi030@gmail.com>
2023-10-03 22:43:53 +0530

    initial set up

---
+++ my-food-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h4>My First App</h4>
+      </header>
+    </div>
+  );
+}
+
+export default App;
