6ce071883f307425c689867da52b3da196750624
pavanvaddadi <pavanvaddadi030@gmail.com>
2023-10-13 00:19:46 +0530

    Added filter and use State logic

--- my-food-app/src/components/Body.js
+++ my-food-app/src/components/Body.js
@@ -1,15 +1,26 @@
 import { ResCard } from "./ResCard";
 import { resObj } from "../utils/mock-data";
+import { useState } from "react";
 
 function Body() {
+  let [listOfRes, setListOfRes] = useState(resObj);
+  const filterData = () => {
+    listOfRes = listOfRes.filter((eachRes) => {
+      return eachRes.info.avgRating >= 4.2;
+    });
+    setListOfRes(listOfRes);
+  };
+
   return (
     <div>
       <div className="searchBox">
-        <h4>Search Here</h4>
+        <button className="btn" onClick={filterData}>
+          Top Rated Restaurants
+        </button>
       </div>
       <div className="card-container">
         <div className="res-container">
-          {resObj.map((eachRes) => {
+          {listOfRes.map((eachRes) => {
             return <ResCard key={eachRes.info.id} resData={eachRes.info} />;
           })}
         </div>
